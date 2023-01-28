0b25f5b106d5f62cfad65165e132a448fcda8a9e
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-01-28 17:44:22 +0100

    first commit

---
+++ docs/logic.js
@@ -0,0 +1,32 @@
+let suma = 0;
+let tiradas = 0;
+
+function reintentar() {
+    suma = 0;
+    tiradas = 0;
+    tirar();
+}
+
+function tirar() {
+    const xhttp = new XMLHttpRequest;
+    xhttp.open("GET", "http://172.16.130.215/tom.php");
+    xhttp.send();
+    xhttp.onload = function() {
+        if (xhttp.status == 200) {
+            const data = JSON.parse(xhttp.responseText);
+            const numero = data.numero;
+            suma += numero;
+            document.getElementById("suma").innerHTML = suma;
+            document.getElementById("tiradas").innerHTML += "/n"+numero;
+        } else { console.log("Conexión fallida")}
+    }
+
+}
+
+function plantarse() {
+    if (suma <= 18) {
+        document.getElementById("resultado").innerHTML = '<div id="gallina">Eres un gallina</div>';
+    } else {
+        document.getElementById("resultado").innerHTML = '<div id="gallina">Eres un campeón</div>';
+    }
+}
